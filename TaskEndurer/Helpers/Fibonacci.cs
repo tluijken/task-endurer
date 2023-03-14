273462f19df2be37bffe3b9eaf9d646d94a8972d
Thomas Luijken <tluijken@outlook.com>
2023-03-15 00:34:57 +0100

    Fibonacci to expression body

--- TaskEndurer/Helpers/Fibonacci.cs
+++ TaskEndurer/Helpers/Fibonacci.cs
@@ -11,15 +11,13 @@ internal static class Fibonacci
     /// <returns>
     ///     The Fibonacci number at the specified index.
     /// </returns>
-    public static int CalculateNumberAtIndex(int index)
-    {
-        return index switch
+    public static int CalculateNumberAtIndex(int index) =>
+        index switch
         {
             < 0 => throw new ArgumentOutOfRangeException(nameof(index), "The index must be greater than or equal to zero."),
             0 => 0,
             _ => Enumerable.Range(0, index + 1).Aggregate((previous: 0, current: 1), (previousNumbers, current) =>
-                current < 2 ? previousNumbers : (previousNumbers.current, previousNumbers.previous + previousNumbers.current))
-            .current
+                    current < 2 ? previousNumbers : (previousNumbers.current, previousNumbers.previous + previousNumbers.current))
+                .current
         };
-    }
 }
